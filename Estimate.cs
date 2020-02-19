bce911223f28c47ebd165c53483d476a592abdfc
Abraham <abrhuerta@gmail.com>
2020-02-19 12:05:57 -0600

    First Commit, added Estimate class/file. Added variables.

---
+++ Estimate.cs
@@ -0,0 +1,24 @@
+/*
+
+    Author: Abe Huerta
+    Description: This is the estimate class that is used to create an estimate 
+      price of boarding a users dog. This is part of the MPLSDogBoarding.com website.
+    Repository: https://github.com/CWEB1111-1112/mpls-dog-boarding-price-estimate-calculator-assignment-1-1-hueabrw
+*/
+
+
+
+namespace assignment_one{
+
+    class Estimate{
+
+        string owner;
+        string dogName;
+        string dogWeight;
+        int daysOfStay;
+        char serviceAddOnCode;
+        double totalEstimate;
+
+    }
+
+}
