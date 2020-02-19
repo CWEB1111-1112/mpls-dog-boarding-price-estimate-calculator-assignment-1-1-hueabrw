35bbaa9272c50fb1256eb19f965bae66d5b866dc
Abraham <abrhuerta@gmail.com>
2020-02-19 13:17:25 -0600

    added error handling

--- Program.cs
+++ Program.cs
@@ -10,18 +10,60 @@ namespace assignment_one
             Console.WriteLine("\nPlease complete the following form:");
             Console.Write("Your Name: ");
             String owner = Console.ReadLine();
+
             Console.Write("Dogs name: ");
             String dogName = Console.ReadLine();
-            Console.Write("Dogs wieght: ");
-            double dogWeight = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Number of days dog will stay: ");
-            int daysOfStay =  Convert.ToInt32(Console.ReadLine());
-            Console.Write("Service add on: ");
-            char serviceCode = Convert.ToChar(Console.ReadLine().ToUpper());
+            
+            double dogWeight = getWeight();
+
+            int daysOfStay =  getDays();
+
+            char serviceCode = getServiceCode();
 
             Estimate estimate = new Estimate(owner, dogName, dogWeight, daysOfStay, serviceCode);
 
             Console.WriteLine(estimate.ToString());
+
+        }
+        public static double getWeight(){
+            
+            Console.Write("Dogs wieght: ");
+            var input = Console.ReadLine();
+            try{
+                double weight = Convert.ToDouble(input);
+                return weight;
+            }catch{
+                Console.WriteLine("Please enter a valid weight.");
+                return getWeight();
+            }
+        }
+        public static int getDays(){
+            
+            Console.Write("Number of days dog will stay: ");
+            var input = Console.ReadLine();
+            try{
+                int days = Convert.ToInt32(input);
+                return days;
+            }catch{
+                Console.WriteLine("Please enter a valid number of days.");
+                return getDays();
+            }
+        }
+        public static char getServiceCode(){
+            
+            Console.Write("Service add on: ");
+            var input = Console.ReadLine();
+            try{
+                char code = Convert.ToChar(input);
+                if(code != 'N' && code != 'A' && code != 'C'){
+                    throw new System.Exception();
+                }else{
+                    return code;
+                }
+            }catch{
+                Console.WriteLine("Please enter a valid service code (either N, A, or C).");
+                return getServiceCode();
+            }
         }
     }
 }
